d200ae456d9f52ffcd45d1ec7b2f33ecd4d47b97
Gaurav <gauravchumber20@gmail.com>
2025-03-02 02:56:46 -0500

    Nav And Footer In about

--- frontend/src/app/about/page.tsx
+++ frontend/src/app/about/page.tsx
@@ -5,6 +5,7 @@ import Link from "next/link";
 import Image from "next/image";
 import { FaLinkedin, FaGithub } from "react-icons/fa";
 import Footer from "@/components/Footer";
+import Nav from "@/components/Nav";
 
 const teamMembers = [
   {
@@ -50,24 +51,7 @@ export default function AboutPage() {
       </Head>
 
       {/* Navigation Bar */}
-      <nav className="fixed top-0 w-full bg-white text-[#13531c] shadow-md py-4 px-8 flex justify-between items-center z-50">
-        <h1 className="text-2xl font-semibold tracking-wide">HowGood</h1>
-        <ul className="flex space-x-6 text-lg">
-          <li><Link href="/" className="hover:text-gray-600">Home</Link></li>
-          <li><Link href="/about" className="hover:text-gray-600">About</Link></li>
-      {/* <li><Link href="/contact" className="hover:text-gray-600">Contact</Link></li> */}
-          <li>
-            <SignedOut>
-              <div className="bg-[#13531c] text-white px-4 py-2 rounded-md hover:bg-green-700 cursor-pointer">
-                <SignInButton mode="modal" />
-              </div>
-            </SignedOut>
-            <SignedIn>
-              <UserButton afterSignOutUrl="/" />
-            </SignedIn>
-          </li>
-        </ul>
-      </nav>
+      <Nav/>
 
 {/* Website Overview Section */}
       <div className="mt-20 text-center">
@@ -167,6 +151,7 @@ export default function AboutPage() {
           ))}
         </div>
       </div>
+      <Footer/>
     </div>
   );
 }
